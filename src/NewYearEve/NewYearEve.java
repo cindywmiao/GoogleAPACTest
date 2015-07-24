fb760ee9c27a3d26b788a2307ae7c236143f92e0
cindywmiao <miaowang@miaodeMacBook-Air.local>
2015-07-25 07:57:45 +0800

    Round B

---
+++ src/NewYearEve/NewYearEve.java
@@ -0,0 +1,53 @@
+package NewYearEve;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class NewYearEve {
+	public static String path = System.getProperty("user.dir");
+    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		practice("B-small-practice");
+		practice("B-large-practice");
+       
+	}
+	
+	
+	public static void practice(String fileName){
+		String fileNameInput = path + "//RoundB//" + fileName + ".in";
+		String fileNameOutput = path + "//RoundB//" + fileName + ".out";
+		String line = null;
+
+	    try {
+	        	
+	    	 FileReader fileReader = new FileReader(fileNameInput);
+	    	 FileWriter fileWriter = new FileWriter(fileNameOutput);
+
+	    	 BufferedReader bufferedReader = new BufferedReader(fileReader);
+	    	 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+	            
+	    	 line = bufferedReader.readLine();
+	    	 int caseNum = Integer.parseInt(line);
+	            
+	    	 for(int i = 0; i < caseNum; i++){
+	    		 bufferedWriter.write("Case #" + i + ": ");
+	    		 bufferedWriter.newLine();
+	    	 }
+	    	 bufferedReader.close();    
+	    	 bufferedWriter.close();
+	     }
+	     catch(FileNotFoundException ex) {
+	    	 System.out.println("Unable to open file '" + fileNameInput + "'");                
+	     }
+	     catch(IOException ex) {
+	    	 System.out.println("Error reading file '"  + fileNameInput + "'");                   
+	     }
+	}
+
+}
